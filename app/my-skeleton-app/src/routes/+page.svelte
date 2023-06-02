d413beabdbfec50ba8980551bc93a34c13549786
shirey25 <coreyshirey@premiumdesignpartners.com>
2023-06-01 20:01:00 -0400

    2 files modified

--- app/my-skeleton-app/src/routes/+page.svelte
+++ app/my-skeleton-app/src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 
-
   import { localStorageStore } from "@skeletonlabs/skeleton";
   import { writable } from "svelte/store";
 
@@ -9,17 +8,9 @@
     phoneNumber: string;
   }
 
-  const client = new Client();
-
-  client
-    .setEndpoint('https://ilokcuwsofdogojuvlln.supabase.co') // Your API Endpoint
-    .setProject('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlsb2tjdXdzb2Zkb2dvanV2bGxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc5ODY0NDksImV4cCI6MTk5MzU2MjQ0OX0.xYz0J-4M0hVyymwyYKaAMofXxz_i9PX-HCrJrVA6OwY); // Your project ID
-
   let inputName = "";
   let inputPhoneNumber = "";
   let isLoggedIn = false;
-  let email = "";
-  let password = "";
 
   const contactStore = localStorageStore<Contact[]>("contactStore", []);
 
@@ -40,79 +31,62 @@
 
   function clearInput() {
     inputName = "";
-    inputEmail = "";
+    inputPhoneNumber = "";
   }
 
-  async function loginWithGoogle() {
-    try {
-      // Redirect to Google authentication
-      await client.account.createOAuth2Session("google");
-
+  
+  }
   async function signInWithGoogle() {
-  const { data, error } = await supabase.auth.signInWithOAuth({
+    const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
   })
-}
-
-      // Once the user is redirected back to your app, you can check if the session was created
-      const response = await client.account.get();
-
-      if (response.$id) {
-        isLoggedIn = true;
-      }
-    } catch (error) {
-      console.error("Login error:", error);
-    }
+}// Implement the Google Sign-In logic here
+    // You can use the Google Sign-In API or any other method of your choice
+    // Set isLoggedIn to true upon successful sign-in
   }
 
   function logout() {
-    client.account.deleteSession("current");
-    isLoggedIn = false;
+    // Implement the logout logic here
+    // Set isLoggedIn to false and clear any user data or session information
   }
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
   <div class="space-y-5">
-    <h1 class="h1">Contact List</h1>
-    <p>This is an example of a login form with Svelte and Appwrite</p>
+    <h1 class="h1">Login to Dash -GPT</h1>
+    <p>This is an example of a login form with Svelte and Google Sign-In</p>
 
     {#if isLoggedIn}
-    <!-- Contact form -->
-    <label class="label">
-      <span>Name</span>
-      <input class="input" type="text" placeholder="Name" bind:value="{inputName}" />
+      <!-- User is logged in -->
+      <div>
+        <!-- Contact form -->
+        <label class="label">
+          <span>Name</span>
+          <input class="input" type="text" placeholder="Name" bind:value="{inputName}" />
 
-      <span>Email</span>
-      <input class="input" type="Email" placeholder="example@example.com" bind:value="{inputEmail" />
-    </label>
-<!-- Login form -->
-    <div>
-      <button type="button" class="btn variant-ghost" on:click="{signInWithGoogle}">Login with Google</button>
-    </div>
-    <button type="button" class="btn variant-ghost" on:click="{addContact}">Add Contact</button>
-    <hr />
+          <span>Phone Number</span>
+          <input class="input" type="text" placeholder="111-111-1111" bind:value="{inputPhoneNumber}" />
+        </label>
 
-    <h2 class="h2">Your Previous Acct Logins /h2>
-    {#each $contactStore as contact, index}
-    <div class="card p-2">
-      <h3>{contact.name}</h3>
-      <h3>{contact.phoneNumber}</h3>
-      <button type="button" class="btn variant-danger" on:click="{() => deleteContact(index)}">Delete</button>
-    </div>
-    {/each}
+        <button type="button" class="btn variant-ghost" on:click="{addContact}">Add Contact</button>
+        <hr/>
 
-    <button type="button" class="btn variant-ghost" on:click="{logout}">Logout</button>
+        <h2 class="h2">Your Previous Account Logins</h2>
+        {#each $contactStore as contact, index}
+          <div class="card p-2">
+            <h3>{contact.name}</h3>
+            <h3>{contact.phoneNumber}</h3>
+            <button type="button" class="btn variant-danger" on:click="{() => deleteContact(index)}">Delete</button>
+          </div>
+        {/each}
+
+        <button type="button" class="btn variant-ghost" on:click="{logout}">Logout</button>
+      </div>
     {:else}
- 
-   {#if isLoggedIn}
-    <!-- Contact form -->
-    <label class="label">
-      <span>Name</span>
-      <input class="input" type="text" placeholder="Name" bind:value="{inputName}" />
-
-      <span>Phone Number</span>
-      <input class="input" type="text" placeholder="111-111-1111" bind:value="{inputPhoneNumber}" />
-    </label>
+      <!-- User is not logged in -->
+      <div>
+        <button type="button" class="btn variant-ghost" on:click="{signInWithGoogle}">Login with Google</button>
+      </div>
     {/if}
   </div>
 </div>
